3cc56cf1934ad676cd05e026cade7d63c0b05918
ｌｉｌａｈ ▸ 🎀 <xxami@users.noreply.github.com>
2018-11-04 18:13:34 +0000

    add ioc/yaml depends

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
 ﻿namespace Squishy3
 {
     using System;
+    using MicroResolver;
+    using SharpYaml;
 
     public static class Program
     {
         [STAThread]
         static void Main()
         {
+            //var container = TinyIoCContainer.Current;
+            //container.AutoRegister();
+
             using (var game = new Squishy3())
                 game.Run();
         }
