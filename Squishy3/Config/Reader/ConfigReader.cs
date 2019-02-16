1718abb0055c48b4aa4b2bf5b041a32c60412728
『ｌｉｌａｈ』 <xxami@users.noreply.github.com>
2019-02-16 18:36:52 +0000

    remove fixie, use standard xunit tests

--- Squishy3/Config/Reader/ConfigReader.cs
+++ Squishy3/Config/Reader/ConfigReader.cs
@@ -1,25 +1,33 @@
 
-namespace Squishy3.Config.Reader {
+using System.Reflection.Metadata;
 
-    using System.IO;
-    using System.IO.Abstractions;
-    using SharpYaml;
-    using SharpYaml.Serialization;
+namespace Squishy3.Config.Reader
+{
+  using System.IO;
+  using System.IO.Abstractions;
+  using SharpYaml;
+  using SharpYaml.Serialization;
 
-    public class ConfigReader {
-        private IFileSystem _iosys;
+  public class ConfigReader
+  {
+    private IFileSystem _iosys;
 
-        public ConfigReader(IFileSystem iosys) {
-            _iosys = iosys;
-        }
-        public T Read<T>(string configName) {
-            using (var input = _iosys.File.OpenRead(configName)) {
-                var serializer = new Serializer();
-                using (var reader = new StreamReader(input)) {
-                    var config = serializer.Deserialize<T>(reader);
-                    return config;
-                }
-            }
-        }
+    public ConfigReader(IFileSystem iosys)
+    {
+      _iosys = iosys;
     }
+
+    public T Read<T>(string configName)
+    {
+      using (var input = _iosys.File.OpenRead(configName))
+      {
+        var serializer = new Serializer();
+        using (var reader = new StreamReader(input))
+        {
+          var config = serializer.Deserialize<T>(reader);
+          return config;
+        }
+      }
+    }
+  }
 }
